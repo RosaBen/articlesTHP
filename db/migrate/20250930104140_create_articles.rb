33a77c8fb6cd2c71a0fcfe4b9b6c090720ccda98
RosaBen <benchabane.rosa@outlook.com>
2025-09-30 16:21:25 +0200

    updated users

--- db/migrate/20250930104140_create_articles.rb
+++ db/migrate/20250930104140_create_articles.rb
@@ -1,8 +1,10 @@
+
 class CreateArticles < ActiveRecord::Migration[8.0]
   def change
     create_table :articles do |t|
       t.string :title
       t.text :content
+      t.references :user, null:false, foreign_key:true
 
       t.timestamps
     end

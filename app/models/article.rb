1566ee7bbd07ffa50e00ea27bc7a4a0c4d1291cb
RosaBen <benchabane.rosa@outlook.com>
2025-10-01 04:03:21 +0200

    added phottos

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
  belongs_to :user
  has_many :comments, dependent: :destroy
+ has_many_attached :photos
 
  validates :title, presence: true
  validates :content, presence: true, length: { minimum: 10 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
 require "faker"
+require "open-uri"
 
+puts "Nettoyage de la base..."
+Comment.destroy_all
 Article.destroy_all
 User.destroy_all
 
-ActiveRecord::Base.connection.reset_pk_sequence!('articles')
-ActiveRecord::Base.connection.reset_pk_sequence!('users')
+ActiveRecord::Base.connection.reset_pk_sequence!("users")
+ActiveRecord::Base.connection.reset_pk_sequence!("articles")
+ActiveRecord::Base.connection.reset_pk_sequence!("comments")
 
+
+puts "Création des utilisateurs..."
 password = 'password'
 
 user = User.create!(username: 'rosa', email: 'test@test.com', password: password, password_confirmation: password)
@@ -18,11 +24,28 @@ end
 
 allUsers = User.all
 
+puts "Création des articles avec images..."
+
 30.times do
-  Article.create!(
+  article = Article.create!(
     title: Faker::Book.title,
     content: Faker::JapaneseMedia::StudioGhibli.quote + " " + Faker::TvShows::Friends.quote + " " + Faker::TvShows::SouthPark.quote,
     user: allUsers.sample,
     is_private: [true, false].sample
   )
+
+  rand(1..3).times do
+    file = URI.open("https://picsum.photos/800/600")
+    article.photos.attach(io: file, filename: "photo.jpg", content_type: "image/jpg")
+  end
+
+    rand(1..5).times do
+    Comment.create!(
+      content: Faker::Quote.famous_last_words,
+      user: allUsers.sample,
+      article: article
+    )
+  end
 end
+
+puts "Seeds terminés ✅"
